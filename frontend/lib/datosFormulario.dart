f5e46e058e3cb8785e5dd9bf896bdb5e564ba4d1
Gustavo valdez <valdezgustavo461@gmail.com>
2021-06-28 19:24:47 -0500

    cree la pagiina donde muestra el comentario y compuse los controladores de la base de datos

--- frontend/lib/datosFormulario.dart
+++ frontend/lib/datosFormulario.dart
@@ -5,8 +5,76 @@ class DatosFormulario extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
-    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Usuario',
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center),
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            title: Text(
+              'Id del Usuario',
+              style: TextStyle(
+                fontSize: 17.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            leading: Icon(
+              Icons.person_add_alt,
+              color: Colors.black,
+            ),
+            subtitle: Text(
+              user.id,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'Nombre del Usuario',
+              style: TextStyle(
+                fontSize: 17.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            leading: Icon(
+              Icons.person,
+              color: Colors.black,
+            ),
+            subtitle: Text(
+              user.name,
+              style: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Divider(),
+          ListTile(
+            title: Text(
+              'Correo del Usuario',
+              style: TextStyle(
+                fontSize: 17.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            leading: Icon(
+              Icons.email,
+              color: Colors.black,
+            ),
+            subtitle: Text(
+              user.email,
+              style: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    )
   }
 }
